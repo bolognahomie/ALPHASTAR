c33cbe731cd249eaff331ead479e61cfd8794521
loganbru <loganbrunnerr@gmail.com>
2022-02-14 19:14:58 -0500

    player explodes on impact

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -6,6 +6,12 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
   [SerializeField] float loadLevelDelay = 1f;
+  [SerializeField] ParticleSystem crashParticles;
+
+//   void Start()
+//   {
+//     Debug.Log(gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject);
+//   }
 
   void OnTriggerEnter(Collider other)
   {
@@ -14,7 +20,7 @@ public class CollisionHandler : MonoBehaviour
 
   void ProcessCrash()
   {
-    Debug.Log($"{this.name} collided with something 2");
+    destroyPlane();
     GetComponent<PlayerControls>().enabled = false;
     Invoke("ReloadLevel", loadLevelDelay);
   }
@@ -23,5 +29,21 @@ public class CollisionHandler : MonoBehaviour
   {
     int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
     SceneManager.LoadScene(currentSceneIndex);
+  }
+
+  void destroyPlane()
+  {
+    crashParticles.Play();
+    gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+    gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>().Stop();
+    gameObject.transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.transform.GetChild(1).gameObject.GetComponent<ParticleSystem>().Stop();
+    
+    gameObject.transform.GetChild(0).gameObject.transform.GetChild(1).transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
+    gameObject.transform.GetChild(0).gameObject.transform.GetChild(2).transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
+    gameObject.transform.GetChild(0).gameObject.transform.GetChild(3).transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
+    gameObject.transform.GetChild(0).gameObject.transform.GetChild(4).transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
+    gameObject.transform.GetChild(0).gameObject.transform.GetChild(5).transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
+    gameObject.transform.GetChild(0).gameObject.transform.GetChild(6).transform.GetChild(0).gameObject.GetComponent<MeshRenderer>().enabled = false;
   }
 }

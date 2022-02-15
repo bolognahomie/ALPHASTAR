fd96c28624477da71f85b906037826bcad2441e7
loganbru <loganbrunnerr@gmail.com>
2022-02-14 19:40:04 -0500

    enemy explosion FX

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+
    void OnParticleCollision(GameObject other)
    {
-       Debug.Log("You killed got a kill");
+       GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+       vfx.transform.parent = parent;
        Destroy(gameObject);
    }
 }
